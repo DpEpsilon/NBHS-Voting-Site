ae2a0b7636ebb370bacff0b7b0d31cb913ad5d90
Daniel Phillips <dyproepsilon@gmail.com>
2013-05-23 18:52:22 +1000

    added get_user function

--- user.py
+++ user.py
@@ -1,5 +1,5 @@
 import sqlite3
-import databse_engine
+from databse_engine import get_db_connection
 
 class User(object):
 	def __init__(self, userid, password, firstname, lastname,\
@@ -20,4 +20,42 @@ class NomineeInfo(object):
 	def __init__(self, experience, why):
 		self.experience = experience
 		self.why = why
+
+def get_user(user):
+	if user is None:
+		return None
+		
+	connection = get_db_connection()
+	cursor = connection.cursor()
+	if type(user) == int:
+		cursor.execute("select userid, username, password, firstname, lastname "
+					   "from users where userid = ?", (user,))
+	elif isinstance(user, basestring):
+		cursor.execute("select userid, username, password, firstname, lastname "
+					   "from users where username = ?", (user,))
+	else:
+		raise TypeError("user argument passed to get_user must "
+						"be and integer or a string or None.")
+
+	user_table_row = cursor.fetchone()
+	if user_table_row is None:
+		return None
+
+	userid = user_table_row[0]
+	
+	cursor.execute("select year, house from students "
+				   "where userid = ?", (userid,))
+	student_table_row = cursor.fetchone()
+	cursor.execute("select experience, why from nominees "
+				   "where userid = ?", (userid,))
+	nominees_table_row = cursor.fetchone()
+
+	return User(user_table_row[0],
+				user_table_row[1],
+				user_table_row[2],
+				user_table_row[3],
+				StudentInfo(student_table_row[0],
+							student_table_row[1]),
+				NomineeInfo(nominees_table_row[0],
+							nominees_table_row[1]))
 
