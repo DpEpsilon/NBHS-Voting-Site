565cd679edb0965c45f2d211c9f04f95bb35349c
Daniel Phillips <dyproepsilon@gmail.com>
2013-06-12 21:10:20 +1000

    delete log, in case of accidents.

--- submissions.py
+++ submissions.py
@@ -15,3 +15,21 @@ def add_nominee_field(userid, field, submission):
 	cursor.execute("""insert into nominee_fields (userid, field, submission)
 values (?, ?, ?);""", (userid, field, submission))
 	connection.commit()
+
+def delete_nominee_field(userid, field):
+	connection = database_engine.get_db_connection()
+	cursor = connection.cursor()
+	cursor.execute("""select submission from nominee_fields
+ where userid = ? and field = ?""", (userid, field))
+
+	old_submission = cursor.fetchone()
+	if old_submission is not None:
+		deletelog = open('deletelog.txt', 'a')
+		deletelog.write(str(userid) + ',' + field + ':\n' +
+						repr(old_submission[0]) + '\n')
+		del deletelog
+	
+	cursor.execute("""delete from nominee_fields
+ where userid = ? and field = ?""", (userid, field))
+	connection.commit()
+	
