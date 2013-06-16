fbc18098429adaab5f564eda668d45511d653155
Daniel Phillips <dyproepsilon@gmail.com>
2013-06-16 22:41:53 +1000

    make sure checkboxes start unchecked

--- js/voting.js
+++ js/voting.js
@@ -1,4 +1,11 @@
 var count = 0;
+boxes = $('.vote_box');
+
+for (var i = 0; i < boxes.length; i++) {
+	// Just to make sure
+	boxes[i].checked = false;
+}
+
 function changed_vote(cb) {
 	if (cb.checked) {
 		count += 1;
